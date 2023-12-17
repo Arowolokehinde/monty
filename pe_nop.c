53bc4b9866dd44a71761e8bc40506fc2d24283c6
Pelile23 <pelile.chintimbwe@gmail.com>
2023-12-17 01:52:21 -0800

    does nothing

---
+++ pe_nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ *k_nop- Points to nothing
+ *@stack: Points to the stack head
+ *@number: Points to the first line_number
+ * Written by Pelile Chintimbwe
+ *Return: Always success
+ */
+void k_nop(stack_t **stack, unsigned int number)
+{
+	(void) number;
+	(void) stack;
+}
---
+++ pe_pall.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * print_to_pall - function that prints the stack
+ * @stack: Points to the stack head
+ * @number: The line not used
+ * Return: Always success
+ * Written by: Pelile Chintimbwe and Kehinde Arowolo
+ */
+void print_to_pall(stack_t **stack, unsigned int number)
+{
+        stack_t *temp;
+        (void)number;
+
+        temp = *stack;
+        if (temp == NULL)
+                return;
+
+        while (temp)
+        {
+                printf("%d\n", temp->n);
+                temp = temp->next;
+        }
+}
